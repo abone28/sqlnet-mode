5429f7f436331b59b29521ab3821d3eaa851a80e
Andrey Bondarenko <abone27@mail.ru>
2022-01-10 15:03:16 +0500

    add a license

--- sqlnet-mode.el
+++ sqlnet-mode.el
@@ -1,4 +1,26 @@
-;;; sqlnet-mode.el --- Major mode for editing sqlnet/listener/tnsnames.ora
+;;; sqlnet-mode.el --- Major mode for editing tnsnames.ora and others
+
+;; Copyright (c) 2019, 2022 Andrey Bondarenko
+
+;; Author: Andrey Bondarenko
+;; Maintainer: Andrey Bondarenko
+;; Homepage: https://github.com/abone28/sqlnet-mode
+;; License: GPL-3.0-or-later
+;; Keywords: languages, faces
+;; Version: 0.0.1
+
+;; sqlnet-mode.el is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; It is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
